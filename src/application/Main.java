350c6b29986992506c1830927aa52943788ca696
Thiago Seidel <thiagosseidel@gmail.com>
2021-02-28 00:10:57 -0300

    #5 About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,6 +10,13 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+	private static Scene mainScene;
+	
+	public static Scene getMainScene() {
+		
+		return mainScene;
+	}
+	
 	@Override
 	public void start(Stage primaryStage) {
 		
@@ -22,7 +29,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			
 			primaryStage.setScene(mainScene);
 			//primaryStage.setMaximized(true);
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,21 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
 
 public class MainViewController implements Initializable {
 
@@ -31,15 +41,39 @@ public class MainViewController implements Initializable {
 	}
 	
 	@FXML
-	private void onMenuItemAboutAction() {
+	private synchronized void onMenuItemAboutAction() {
 		
-		System.out.println("onMenuItemAboutAction");
+		loadView("/gui/About.fxml");
 	}
 	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 
 		
+	}
+	
+	private void loadView(String absoluteName) {
+		
+		try
+		{
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+		}
+		catch(IOException e) {
+			
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
